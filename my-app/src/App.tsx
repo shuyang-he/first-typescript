215f788bf637db4aca90c24ae658b76263d81757
Shuyang He <maxhe2019@gmail.com>
2021-02-07 23:41:35 -0800

    fetch pokemon paganation

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import style from "./App.module.scss";
 
 function App() {
+  const [data, setData] = useState<any>({});
+  const [offset, setOffset] = useState<number>(0);
+  const [limit, setLimit] = useState<number>(20);
+
+  useEffect(() => {
+    const pokemonAPI: string = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+    fetch(pokemonAPI)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setData(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [offset]);
+
+  const handlePrev = () => {
+    setOffset(offset - limit);
+  };
+
+  const handleNext = () => {
+    setOffset(offset + limit);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={`container ${style.App}`}>
+      <ul className={style.list}>
+        {data && data.results
+          ? data.results.map((item: any, index: number) => {
+              return (
+                <li key={index} className={style.list__item}>
+                  <h4>{item.name}</h4>
+                </li>
+              );
+            })
+          : null}
+      </ul>
+      <div className={style.button_con}>
+        <button
+          className={`${offset === 0 ? style.button_con__item__inactive : ``} ${
+            style.button_con__item
+          }`}
+          onClick={handlePrev}
         >
-          Learn React
-        </a>
-      </header>
+          <h4>Prev</h4>
+        </button>
+        <button className={style.button_con__item} onClick={handleNext}>
+          <h4>Next</h4>
+        </button>
+      </div>
     </div>
   );
 }
